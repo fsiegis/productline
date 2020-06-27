e23c54ede8d46d4b50799d76736146db3e32ee41
finn <finn@myhost>
2020-06-27 14:07:10 +0200

    OtherGUI as Feature

--- BookManagement-Task4/features/OtherGUI/src/OtherGUI.java
+++ BookManagement-Task4/features/OtherGUI/src/OtherGUI.java
@@ -4,15 +4,15 @@ import interfaces.IGUI;
 
 import java.awt.*;
 
-public class OneGUI implements IGUI {
+public class OtherGUI implements IGUI {
 
 	public JFrame getFrame(String title) {
 		JFrame newFrame = new JFrame();
 		newFrame.setTitle(title);
 		newFrame.setLayout(new BorderLayout());
-		newFrame.setBackground(Color.white);
 		newFrame.setLocation(50,0);
-		newFrame.setSize(700,600);
+		newFrame.setSize(1000,1000);
+		newFrame.setBackground(Color.red);
 		newFrame.setMinimumSize(new Dimension(300,250));
 		return newFrame;
 	}
